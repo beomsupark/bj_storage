266ca75eca91c7f733ea7ce5d4565ee3ef05f4fb
beomsupark <77788646+beomsupark@users.noreply.github.com>
2021-02-09 15:56:48 +0900

    Add files via upload

---
+++ bj_5585.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj_5585 {
+
+	
+	public static void main(String args[]) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		int num = 1000 - Integer.parseInt(s);
+		int[] coin = {500,100,50,10,5,1};
+		int count =0;
+		for(int i=0;i<coin.length;i++)
+		{
+			while(num>=coin[i])
+			{
+				num = num - coin[i];
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+	
+}
